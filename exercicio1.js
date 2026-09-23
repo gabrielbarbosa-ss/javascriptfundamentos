adf9ba755015f4ff5bda76cc35d372c44f7ad6b4
Gabriel <gabrielbarbosasoares2007@gmail.com>
2026-09-22 23:12:42 -0300

    Create exercicio1.js

---
+++ exercicio1.js
@@ -0,0 +1,17 @@
+//Variáveis e tipos: Crie variáveis para nome, idade e se é estudante (boolean). Imprima uma frase usando template literals(crase para strings)
+
+
+let nome = "Gabriel";
+let idade = 19;
+const estudante = true;
+
+console.log(`O aluno ${nome} que tem ${idade} anos é estudante?`)
+console.log(`É Estudante: ${estudante}`);
+
+let nome2 = "João";
+let idade2 = 23;
+const estudante2 = false;
+
+console.log(`O aluno ${nome2} que tem ${idade2} anos é estudante?`)
+console.log(`É Estudante: ${estudante2}`);
+
