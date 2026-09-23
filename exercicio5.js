c02c8b06879f52baca929c12ba1f5dddd3f5c736
Gabriel <gabrielbarbosasoares2007@gmail.com>
2026-09-22 23:12:50 -0300

    Create exercicio5.js

---
+++ exercicio5.js
@@ -0,0 +1,12 @@
+// Loops (while): Escreva um loop que some os números de 1 até um valor n informado.
+
+const valorinformado = 20;
+let soma = 0;
+let i = 1;
+while(i<=valorinformado){
+    soma+=i
+    i++
+}
+
+console.log(soma)
+
